c0c2fade696b583d0b111fb759b5b08d271d1c0d
KoRnaholic <114020851+KoRnaholic@users.noreply.github.com>
2024-03-13 15:57:13 +0400

    added artice component and modified code

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -6,7 +6,7 @@ export default function Button({ children, forBanner }) {
       <button
         className={`${
           forBanner
-            ? "py-3 px-7 mt-8 bg-slate-700 hover:bg-slate-800  rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300"
+            ? "py-3 px-7 mt-8 bg-slate-700 hover:bg-slate-800 rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300"
             : "py-2 px-10 bg-slate-50 hover:bg-slate-200"
         } `}
       >
---
+++ src/components/content/Article.js
@@ -0,0 +1,17 @@
+export default function Article({ article }) {
+  return (
+    <>
+      <div className="py-4 flex flex-col items-center justify-center">
+        <div className="p-4 bg-slate-100 w-2/3 rounded-lg shadow-lg transform transition-transform hover:scale-105">
+          <div className="flex flex-col gap-4 items-start font-sans text-slate-700">
+            <h2 className="text-2xl font-bold">{article.title}</h2>
+            <p className="text-sm">{article.text}</p>
+            <button className="bg-slate-600 hover:bg-slate-800 text-white font-bold py-1 px-3 rounded">
+              Read More
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
