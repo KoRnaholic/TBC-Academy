39a63a7a30467dc4362fdb3c11696df2856f9aea
KoRnaholic <114020851+KoRnaholic@users.noreply.github.com>
2024-03-15 19:29:30 +0400

    added key props

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -3,23 +3,24 @@ import Button from "../UI/Button";
 export default function ProductCard({ products }) {
   return (
     <>
-      {products.map((product) => {
+      {products.map((product, index) => {
         const { title, description, image } = product;
         return (
           <div
-            class="max-w-[250px] max-h-[350px] mx-auto bg-slate-50 shadow-lg rounded-lg  transition ease-in-out delay-150
+            key={index}
+            className="max-w-[250px] max-h-[350px] mx-auto bg-slate-50 shadow-lg rounded-lg  transition ease-in-out delay-150
           hover:-translate-y-1 hover:scale-110 duration-300"
           >
             <img
-              class="w-full  object-cover object-center"
+              className="w-full  object-cover object-center"
               src={image}
               alt="product"
             />
-            <div class="p-4">
-              <h2 class="text-gray-900 font-bold text-xl mb-2">{title}</h2>
-              <p class="text-gray-700 text-base">{description}</p>
-              <div class="mt-3 flex items-center justify-between">
-                <span class="text-gray-900 font-bold">$119.99</span>
+            <div className="p-4">
+              <h2 className="text-gray-900 font-bold text-xl mb-2">{title}</h2>
+              <p className="text-gray-700 text-base">{description}</p>
+              <div className="mt-3 flex items-center justify-between">
+                <span className="text-gray-900 font-bold">$119.99</span>
                 <Button>Add to Cart</Button>
               </div>
             </div>
