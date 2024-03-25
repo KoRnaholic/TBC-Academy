5f00346e0bf3b5cb8a1c58c128e1630be903b3d6
KoRnaholic <114020851+KoRnaholic@users.noreply.github.com>
2024-03-25 21:02:20 +0400

    changed style

--- src/components/content/Article.jsx
+++ src/components/content/Article.jsx
@@ -6,7 +6,7 @@ export default function Article({ article }) {
           <div className="flex flex-col gap-2 items-start font-sans text-slate-700">
             <h2 className="text-2xl font-bold">{article.title}</h2>
             <img src={article.url} alt="laptop" height="100px" width="100px" />
-            <p>Created at :{article.date}</p>
+            <p>Created at: {article.date}</p>
             <p className="text-sm">{article.text}</p>
             <button className="bg-slate-600 hover:bg-slate-800 text-white font-bold py-1 px-3 rounded">
               Read More
