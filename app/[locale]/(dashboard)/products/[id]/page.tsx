d8739aae8406f490207d1d30613a3b96232fbf86
Mariam Khutuashvili <m_khutuashvili@yahoo.com>
2024-04-24 19:18:53 +0400

    Initialize typescript for products route and SingleProduct component

--- app/[locale]/(dashboard)/products/[id]/page.tsx
+++ app/[locale]/(dashboard)/products/[id]/page.tsx
@@ -2,9 +2,31 @@ import SingleProduct from "../../../../../components/main-product/SingleProduct"
 import { fetchProducts } from "../../../../../utils/helpers";
 const URL = "https://dummyjson.com/products";
 
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
+
+interface ProductsResponse {
+  products: Product[];
+}
+
+interface Params {
+  id: string;
+}
+
 export async function generateStaticParams() {
   const response = await fetch(URL);
-  const products = await response.json();
+  const products: ProductsResponse = await response.json();
 
   const paths = products.products.map((product) => ({
     params: { id: `/products/${product.id}` },
@@ -13,7 +35,7 @@ export async function generateStaticParams() {
   return paths;
 }
 
-export default async function Products({ params }) {
+export default async function Products({ params }: { params: Params }) {
   const data = await fetchProducts(URL, params.id);
 
   return <SingleProduct data={data} />;
