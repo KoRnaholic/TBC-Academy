8bee729a6389416807fbe745c6f8265aeb1e8892
KoRnaholic <114020851+KoRnaholic@users.noreply.github.com>
2024-05-02 00:02:32 +0400

    starting to refactor blog page

--- app/[locale]/(dashboard)/blog/page.tsx
+++ app/[locale]/(dashboard)/blog/page.tsx
@@ -7,10 +7,23 @@ export default async function Blog() {
   const data: BlogObject = await response.json();
 
   return (
-    <div className="overflow-y-auto max-h-[550px]">
-      {data.recipes.map((blog, index) => {
-        return <Article key={index} blogs={blog} />;
-      })}
-    </div>
+    <>
+      <div className="bg-[#07212e] flex items-center justify-center h-[450px]">
+        <div className="flex-col gap-8  flex ">
+          <h1 className="text-[#F28123] tracking-widest text-xl">
+            WE DELIVER TECH DREAMS
+          </h1>
+          <h1 className="text-center text-white text-4xl sm:text-5xl ">
+            Blog Page
+          </h1>
+        </div>
+      </div>
+
+      <div className="overflow-y-auto max-h-[550px]">
+        {data.recipes.map((blog, index) => {
+          return <Article key={index} blogs={blog} />;
+        })}
+      </div>
+    </>
   );
 }
