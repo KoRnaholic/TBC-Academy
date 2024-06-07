bc33fe56dd16946488be9d032fe95505052af77c
KoRnaholic <114020851+KoRnaholic@users.noreply.github.com>
2024-06-07 23:27:45 +0400

    changed base url once again

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -3,7 +3,7 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth({
   login: handleLogin({
-    returnTo: `${process.env.BASE_URL}/api/login-user`,
+    returnTo: `${process.env.AUTH0_BASE_URL}/api/login-user`,
     authorizationParams: {
       prompt: "login", // Forces the user to always log in
     },
